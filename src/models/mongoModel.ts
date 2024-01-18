1b73e8b06d5b25b993f6851798b0a4a157c433fc
mottak <kissylamottapereira@gmail.com>
2024-01-18 16:52:56 -0300

    chore: cria camada de model

---
+++ src/models/mongoModel.ts
@@ -0,0 +1,22 @@
+import { Model, Schema, models, model } from 'mongoose';
+import IModel from '../interfaces/IModel'
+import { CustomError } from '../helper/CustomError';
+
+export default class MongoModel<T> implements IModel<T> {
+  private schema: Schema;
+  private model: Model<T>;
+
+  constructor(schema: Schema, model: Model<T>) {
+    this.schema = schema;
+    this.model = model;
+    
+  }
+
+  public async create(obj: T) {
+    return this.model.create({...obj});
+  }
+
+  public async getById(id: string) {
+    return this.model.findById(id);
+  }
+}
