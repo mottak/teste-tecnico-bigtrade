7995145852bf6c4cf08db541ca3b896a830414bd
mottak <kissylamottapereira@gmail.com>
2024-01-18 16:53:43 -0300

    chore: cria middleware de erro

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../helper/CustomError';
+import { MongoServerError } from 'mongodb';
+
+export const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+  if (err instanceof CustomError) {
+    return res.status(err.status).json({ message: err.message });
+  }
+
+  if(err instanceof MongoServerError) {
+    return res.status(402).json({ message: err.message})
+
+  }
+
+  res.status(500).json({ error: 'Erro interno do servidor' });
+};
