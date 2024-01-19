5eaba7c66a5602a2782ac48da129cbc6b4bfa5c0
mottak <kissylamottapereira@gmail.com>
2024-01-18 21:21:26 -0300

    refact: separa o schema

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,27 +1,12 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
+import { model } from 'mongoose';
 import UserODM from '../models/user';
 import UserService from '../services/userService'
 import UserController from '../controllers/userController'
-import { Schema, model } from 'mongoose';
-import IUser from '../interfaces/IUser';
+import userSchema from '../models/schemas/userSchema'
 
 const userRouter = Router();
 
-const userSchema= new Schema<IUser>({
-  displayName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-},
-{
-  toJSON: {
-  transform: function (doc, ret) {
-    // Retira a propriedade `_id` e adiciona `_d`
-    ret.id = ret._id.toString();
-    delete ret._id;
-    delete ret.__v;
-    return ret;
-  },
-}, });
 
 
 
@@ -30,5 +15,5 @@ const userService = new UserService(userODM)
 const userController = new UserController(userService)
 
 userRouter.post('/users', async (req, res) => userController.create(req, res));
-
+userRouter.get('/users/:id', async (req, res) => userController.getById(req, res));
 export default userRouter;
