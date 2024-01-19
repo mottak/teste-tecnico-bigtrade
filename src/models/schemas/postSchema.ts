93bda356c51323c6a4517186bd88b302e098c7f2
mottak <kissylamottapereira@gmail.com>
2024-01-19 19:40:09 -0300

    chore: cria schema de post para o banco de dados

---
+++ src/models/schemas/postSchema.ts
@@ -0,0 +1,21 @@
+import { Schema } from 'mongoose';
+import IPost from '../../interfaces/IPost';
+
+const userSchema = new Schema<IPost>({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  userId: { type: String, required: true },
+  published: { type: Date, required: true },
+  updated: { type: Date, required: true }
+},
+{
+  toJSON: {
+  transform: function (doc, ret) {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
+}, });
+
+export default userSchema
